15d58c826f4c41dcb6a4d47010e53540e408f4d1
Vagner Kuntz <30324117+vagnerkuntz@users.noreply.github.com>
2018-01-17 11:38:38 +0000

    Update acordo.php

--- htdocs/site/acordo.php
+++ htdocs/site/acordo.php
@@ -1,26 +1,23 @@
 <?php
-      include ("include.inc.php");
-            $ptitle="Acordo - $cfg[server_name]";
-      include ("header.inc.php");
+	include 'include.inc.php';
+	$ptitle = 'Acordo - ' . $cfg['server_name'];
+    include 'header.inc.php';
 ?>
 
 <div id="content">
 	<div class="top">Acordo entre Damas e Cavalheiros</div>
 	<div class="mid">
-        <p><font class=f2>Como informamos ao entrar no site, Eagle World é um Open Tibia em fase de desenvolvimento, ou seja, bugs acontecem sim e são sempre corrigidos mais cedo ou mais tarde. Entendendo como funciona nosso projeto, precisam estar de acordo com alguns items antes de aventurar-se neste game:</font></p>
+        <p><font class=f2>Como informamos ao entrar no site, Eagle World Ã© um Open Tibia em fase de desenvolvimento, ou seja, bugs acontecem sim e sÃ£o sempre corrigidos mais cedo ou mais tarde. Entendendo como funciona nosso projeto, precisam estar de acordo com alguns items antes de aventurar-se neste game:</font></p>
             
-        <p><font class=f2>1ª)</font><font class=f1> Precisam estar cientes de que o Eagle está sempre passando por constantes modificações, ou seja, se hoje você foi beneficiado em algo, amanhã pode ser que não seja mais. Tudo é sempre em pró do equilíbrio do server.</font></p>
-        <p><font class=f2>2ª)</font><font class=f1> Ao encontrar um bug tanto no server quanto no mapa, precisam reportá-lo a nossa equipe e não tentar se aproveitar dele.</font></p>
-        <p><font class=f2>3ª)</font><font class=f1> Caso venha a morrer ou ser prejudicado por algum bug, precisa estar ciente de que isso pode acontecer e que não temos a obrigação de estar restaurando os personagens. Reportem para que possamos tentar corrigir.</font></p>
-        <p><font class=f2>4ª)</font><font class=f1> Se nosso servidor cai, tanto por ataques a nossa conexão (DDoS), quanto por bugs in game, também precisam estar cientes de que ninguém está livre deste tipo de dificuldade, porém nunca deixaremos de trabalhar para que isto ocorra cada vez menos, perfeição é inalcansável, mas estabilidade é algo que podemos adquirir.</font></p>
-		<p><font class=f2>5ª)</font><font class=f1> Itens ou níveis perdidos não são recuperados.</font></p>
+        <p><font class=f2>1Âª)</font><font class=f1> Precisam estar cientes de que o Eagle estÃ¡ sempre passando por constantes modificaÃ§Ãµes, ou seja, se hoje vocÃª foi beneficiado em algo, amanhÃ£ pode ser que nÃ£o seja mais. Tudo Ã© sempre em prÃ³ do equilÃ­brio do server.</font></p>
+        <p><font class=f2>2Âª)</font><font class=f1> Ao encontrar um bug tanto no server quanto no mapa, precisam reportÃ¡-lo a nossa equipe e nÃ£o tentar se aproveitar dele.</font></p>
+        <p><font class=f2>3Âª)</font><font class=f1> Caso venha a morrer ou ser prejudicado por algum bug, precisa estar ciente de que isso pode acontecer e que nÃ£o temos a obrigaÃ§Ã£o de estar restaurando os personagens. Reportem para que possamos tentar corrigir.</font></p>
+        <p><font class=f2>4Âª)</font><font class=f1> Se nosso servidor cai, tanto por ataques a nossa conexÃ£o (DDoS), quanto por bugs in game, tambÃ©m precisam estar cientes de que ninguÃ©m estÃ¡ livre deste tipo de dificuldade, porÃ©m nunca deixaremos de trabalhar para que isto ocorra cada vez menos, perfeiÃ§Ã£o Ã© inalcansÃ¡vel, mas estabilidade Ã© algo que podemos adquirir.</font></p>
+		<p><font class=f2>5Âª)</font><font class=f1> Itens ou nÃ­veis perdidos nÃ£o sÃ£o recuperados.</font></p>
 			
-        <p><font class=f2>Se aceita estas condições e acredita que estamos tentando sempre fazer o melhor para vocês jogadores, clique <a href="account.php">AQUI</a> e faça sua conta!</p>
+        <p><font class=f2>Se aceita estas condiÃ§Ãµes e acredita que estamos tentando sempre fazer o melhor para vocÃªs jogadores, clique <a href="account.php">AQUI</a> e faÃ§a sua conta!</p>
 	</div>
 	<div class="bot"></div>
 </div>
 
-
-<?php
-	include ("footer.inc.php");
-?>
+<?php include 'footer.inc.php';
