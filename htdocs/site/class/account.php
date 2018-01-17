eb68dbd154ff7adf94d2864d86031106a727a29c
Vagner Kuntz <30324117+vagnerkuntz@users.noreply.github.com>
2018-01-17 11:26:50 +0000

    Update account.php

--- htdocs/site/class/account.php
+++ htdocs/site/class/account.php
@@ -1,34 +1,14 @@
 <?php
-/*
-     Copyright (C) 2007 - 2008  Nicaw
+class Account extends SQL {
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	public $players;
+	private $attrs;
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
-
-class Account extends SQL
-{
-
-public $players, $attrs;
-
-public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 	}
 
-public function load($id)
-	{
+	public function load($id) {
 		if (!is_numeric($id)) return false;
 		//load attributes from database
 		$acc = $this->myRetrieve('accounts', array('id' => $id));
@@ -46,20 +26,19 @@ public function load($id)
 		$this->attrs['location'] = $nicaw_acc['location'];
 		$this->attrs['comment'] = $nicaw_acc['comment'];
 		$this->attrs['recovery_key'] = $nicaw_acc['recovery_key'];
-        if (isset($acc['premdays']) && $acc['premdays'] > 0) $this->attrs['premend'] = $acc['premdays']*24*3600 + time();
-        elseif (isset($acc['premend']) && $acc['premend'] > 0) $this->attrs['premend'] = $acc['premend'];
-		//get characters of this account
-		$this->myQuery('SELECT players.id, players.name FROM players WHERE (`account_id`='.$this->quote($this->attrs['accno']).')');
-		if ($this->failed()) throw new Exception($this->getError());
-		while ($a = $this->fetch_array()){
-			$this->players[] = array('name' => $a['name'], 'id' => $a['id']);
+		if (isset($acc['premdays']) && $acc['premdays'] > 0) $this->attrs['premend'] = $acc['premdays']*24*3600 + time();
+			elseif (isset($acc['premend']) && $acc['premend'] > 0) $this->attrs['premend'] = $acc['premend'];
+			//get characters of this account
+			$this->myQuery('SELECT players.id, players.name FROM players WHERE (`account_id`='.$this->quote($this->attrs['accno']).')');
+			if ($this->failed()) throw new Exception($this->getError());
+			while ($a = $this->fetch_array()){
+				$this->players[] = array('name' => $a['name'], 'id' => $a['id']);
+			}
+			//good, now we have all attributes stored in object
+			return true;
 		}
-		//good, now we have all attributes stored in object
-		return true;
-	}
 
-public function save()
-	{
+	public function save() {
 		$acc['id'] = $this->attrs['accno'];
 		$acc['password'] = $this->attrs['password'];
 		$acc['email'] = $this->attrs['email'];
@@ -68,125 +47,137 @@ public function save()
 		$nicaw_acc['location'] = $this->attrs['location'];
 		$nicaw_acc['comment'] = $this->attrs['comment'];
 		$nicaw_acc['recovery_key'] = $this->attrs['recovery_key'];
-		
-		if (!$this->myReplace('nicaw_accounts',$nicaw_acc))
-			throw new Exception('Cannot save account:<br/>'.$this->getError());
 
-		if ($this->exists()){
-			if (!$this->myUpdate('accounts',$acc, array('id' => $this->attrs['accno'])))
-				throw new Exception('Cannot save account:<br/>'.$this->getError());
-		}else{
-			if (!$this->myInsert('accounts',$acc))
-				throw new Exception('Cannot save account:<br/>'.$this->getError());
+		if (!$this->myReplace('nicaw_accounts', $nicaw_acc)) {
+			throw new Exception('Cannot save account:<br/>' . $this->getError());
+
+			if ($this->exists()) {
+				if (!$this->myUpdate('accounts', $acc, array('id' => $this->attrs['accno']))) {
+					throw new Exception('Cannot save account:<br/>' . $this->getError());
+				}
+			} else {
+				if (!$this->myInsert('accounts', $acc)) {
+					throw new Exception('Cannot save account:<br/>' . $this->getError());
+				}
+			}
+			return true;
 		}
-		return true;
 	}
 
-public function getAttr($attr)
-	{
+	public function getAttr($attr) {
 		return $this->attrs[$attr];
 	}
 
-public function setAttr($attr,$value)
-	{
+	public function setAttr($attr, $value) {
 		$this->attrs[$attr] = $value;
 	}
 
-public function setPassword($new)
-	{global $cfg;
-		$new = $new.$cfg['password_salt'];
-		if ($cfg['password_type'] == 'md5')
+	public function setPassword($new) {
+		global $cfg;
+		$new = $new . $cfg['password_salt'];
+		if ($cfg['password_type'] == 'md5') {
 			$new = md5($new);
-		elseif ($cfg['password_type'] == 'sha1')
+		} elseif ($cfg['password_type'] == 'sha1') {
 			$new = sha1($new);
-		if (empty($new)) throw new Exception('Empty password is not allowed.');
+		}
+		if (empty($new)) {
+			throw new Exception('Empty password is not allowed.');
+		}
 		$this->attrs['password'] = $new;
 	}
 
-public function checkPassword($pass)
-	{global $cfg;
+	public function checkPassword($pass) {
+		global $cfg;
 		$pass = $pass.$cfg['password_salt'];
-		if ($cfg['password_type'] == 'md5')
+		if ($cfg['password_type'] == 'md5') {
 			$pass = md5($pass);
-		elseif ($cfg['password_type'] == 'sha1')
+		} elseif ($cfg['password_type'] == 'sha1') {
 			$pass = sha1($pass);
+		}
 		return $this->attrs['password'] == $pass && !empty($pass);
 	}
 
-public function exists()
-	{
+	public function exists() {
 		$this->myQuery('SELECT * FROM `accounts` WHERE `id` = '.$this->quote($this->attrs['accno']));
-		if ($this->failed()) throw new Exception('Account::exists() cannot determine whether account exists');
-		if ($this->num_rows() > 0) return true;
-		return false;
+		if ($this->failed()) {
+			throw new Exception('Account::exists() cannot determine whether account exists');
+		}
+		if ($this->num_rows() > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-public function logAction($action)
-	{
+	public function logAction($action) {
 		$this->myQuery('INSERT INTO `nicaw_account_logs` (id, ip, account_id, date, action) VALUES(NULL, INET_ATON('.$this->quote($_SERVER['REMOTE_ADDR']).'), '.$this->quote($this->attrs['accno']).', UNIX_TIMESTAMP(NOW()), '.$this->quote($action).')');
-		if ($this->failed())
+		if ($this->failed()) {
 			throw new Exception($this->getError());
+		}
 	}
-	
-public function removeRecoveryKey()
-	{
+
+	public function removeRecoveryKey() {
 		$this->attrs['recovery_key'] = NULL;
 	}
 
-public function addRecoveryKey()
-	{
-		$this->attrs['recovery_key'] = substr(str_shuffle(md5(mt_rand()).md5(time())), 0, 32);
-		$this->logAction('Recovery key added');
+	public function addRecoveryKey() {
+		$this->attrs['recovery_key'] = strtoupper(substr(str_shuffle(md5(mt_rand()).md5(time())), 0, 32));
+		$this->logAction('Recovery key added');	
 		return $this->attrs['recovery_key'];
 	}
 
-public function checkRecoveryKey($key)
-	{
+	public function checkRecoveryKey($key) {
 		return $this->attrs['recovery_key'] === $key && !empty($key);
 	}
 
-public function vote($option)
-	{
+	public function vote($option) {
 		$this->myQuery('INSERT INTO `nicaw_poll_votes` (option_id, ip, account_id) VALUES('.$this->quote($option).', INET_ATON('.$this->quote($_SERVER['REMOTE_ADDR']).'), '.$this->quote($this->attrs['accno']).')');
-		if ($this->failed())
+		if ($this->failed()) {
 			throw new Exception($this->getError());
+		}
 	}
-	
-public function getMaxLevel()
-	{
+
+	public function getMaxLevel(){
 		$this->myQuery('SELECT MAX(level) AS maxlevel FROM `players` WHERE `account_id` = '.$this->quote($this->attrs['accno']));
-		if ($this->failed())
+		if ($this->failed()) {
 			throw new Exception($this->getError);
+		}
 		$row = $this->fetch_array();
 		return (int) $row['maxlevel'];
 	}
 
-public function canVote($option)
-	{
-		$query = 'SELECT nicaw_polls.id FROM nicaw_polls, nicaw_poll_options
-WHERE nicaw_polls.id = nicaw_poll_options.poll_id
-AND nicaw_poll_options.id = '.$this->quote($option).'
-AND '.$this->quote($this->getMaxLevel()).' > minlevel
-AND nicaw_polls.startdate < UNIX_TIMESTAMP(NOW())
-AND nicaw_polls.enddate > UNIX_TIMESTAMP(NOW())';
+	public function canVote($option) {
+		$query = 'SELECT nicaw_polls.id FROM nicaw_polls, nicaw_poll_options WHERE nicaw_polls.id = nicaw_poll_options.poll_id
+			AND nicaw_poll_options.id = ' . $this->quote($option) . '
+			AND ' . $this->quote($this->getMaxLevel()) . ' > minlevel
+			AND nicaw_polls.startdate < UNIX_TIMESTAMP(NOW())
+			AND nicaw_polls.enddate > UNIX_TIMESTAMP(NOW())';
 		$this->myQuery($query);
-		if ($this->failed())
+		if ($this->failed()) {
 			throw new Exception($this->getError);
-		if ($this->num_rows() == 0) return false;
-		if ($this->num_rows() > 1) throw new Exception('Unexpected SQL answer.');
+		}
+		if ($this->num_rows() == 0) {
+			return false;
+		}
+		if ($this->num_rows() > 1) {
+			throw new Exception('Unexpected SQL answer.');
+		}
 		$a = $this->fetch_array();
 		$poll_id = $a['id'];
-		$query = 'SELECT * FROM nicaw_poll_votes, nicaw_poll_options
-WHERE nicaw_poll_options.poll_id = '.$this->quote($poll_id).'
-AND nicaw_poll_options.id = nicaw_poll_votes.option_id
-AND (account_id = '.$this->quote($this->attrs['accno']).' OR ip = INET_ATON('.$this->quote($_SERVER['REMOTE_ADDR']).')
-)';
+		$query = 'SELECT * FROM nicaw_poll_votes, nicaw_poll_options WHERE nicaw_poll_options.poll_id = '.$this->quote($poll_id).'
+			AND nicaw_poll_options.id = nicaw_poll_votes.option_id
+			AND (account_id = '.$this->quote($this->attrs['accno']).' OR ip = INET_ATON('.$this->quote($_SERVER['REMOTE_ADDR']).'))';
 		$this->myQuery($query);
-		if ($this->failed())
+		if ($this->failed()) {
 			throw new Exception($this->getError);
-		if ($this->num_rows() == 0) return true;
-		elseif ($this->num_rows() == 1) return false;
-		else throw new Exception('Unexpected SQL answer.');
+		}
+		if ($this->num_rows() == 0) { 
+			return true;
+		} elseif ($this->num_rows() == 1) {
+			return false;
+		} else {
+			throw new Exception('Unexpected SQL answer.');
+		}
 	}
 }
 ?>
